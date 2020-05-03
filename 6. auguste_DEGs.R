5d4614d0e697c2d9e58c8c5ddab0b82349d1bdb4
lifan36 <64649095+lifan36@users.noreply.github.com>
2020-05-03 18:22:54 -0400

    Update and rename auguste_DEGs.R to 6. auguste_DEGs.R

--- 6. auguste_DEGs.R
+++ 6. auguste_DEGs.R
@@ -24,21 +24,19 @@ setwd("/athena/ganlab/scratch/lif4001/ChaoWang_IKKb/IKKb_KO_Fan/DEGs")
 Cluster_EN_IN <- subset(LG343_IKKbKO_integrated, idents = c("excitatory neurons", "inhibitory neurons"))
 #Subset Microglia
 Cluster_MG <- subset(LG343_IKKbKO_integrated, idents = "microglia")
-Idents(Cluster_MG) <- "orig.ident"
-avg.Cluster_MG <- log1p(AverageExpression(Cluster_MG, verbose = FALSE)$RNA)
-avg.Cluster_MG$gene <- rownames(avg.Cluster_MG)
 
-#Test Cluster_EN_IN and Cluster_MG
-#markers.to.plot <- c("Plp1", "Mbp", "Mobp","Slc17a7", "Nrgn", "Gad1", "Gad2", "Clu", "Aldoc", 
-#                     "Pla2g7", "Cx3cr1", "P2ry12", "Csf1r","Scrg1", "Pdgfra", "Vtn", "Igfbp7",
-#                     "Bnc2", "Slc47a1", "Ttr")
 
-#pdf("Verify_Subset_neurons_MG.pdf", width=12, height=8)
-#DotPlot(Cluster_EN_IN, features = rev(markers.to.plot), cols = c("orange", "plum", "pink", "grey"), dot.scale = 8, 
-#        split.by = "condition") + RotatedAxis()
-#DotPlot(Cluster_MG, features = rev(markers.to.plot), cols = c("orange", "plum", "pink", "grey"), dot.scale = 8, 
-#        split.by = "condition") + RotatedAxis()
-#dev.off()
+Test Cluster_EN_IN and Cluster_MG
+markers.to.plot <- c("Plp1", "Mbp", "Mobp","Slc17a7", "Nrgn", "Gad1", "Gad2", "Clu", "Aldoc", 
+                     "Pla2g7", "Cx3cr1", "P2ry12", "Csf1r","Scrg1", "Pdgfra", "Vtn", "Igfbp7",
+                     "Bnc2", "Slc47a1", "Ttr")
+
+pdf("Verify_Subset_neurons_MG.pdf", width=12, height=8)
+DotPlot(Cluster_EN_IN, features = rev(markers.to.plot), cols = c("orange", "plum", "pink", "grey"), dot.scale = 8, 
+        split.by = "condition") + RotatedAxis()
+DotPlot(Cluster_MG, features = rev(markers.to.plot), cols = c("orange", "plum", "pink", "grey"), dot.scale = 8, 
+        split.by = "condition") + RotatedAxis()
+dev.off()
 
 Idents(Cluster_EN_IN) <- "Condition"
 Idents(Cluster_MG) <- "Condition"
